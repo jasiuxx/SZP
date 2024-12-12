d3cfa60294093fa64e651ddbd87323b71d3ea05f
Jan <waterjan782@gmail.com>
2024-12-12 10:52:03 +0100

    Profil pracownika,skills,bootstrap

--- employees/forms.py
+++ employees/forms.py
@@ -1,19 +1,37 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from .models import CustomUser, Employee
+from .models import CustomUser, Employee, Skill
 
 class EmployeeRegistrationForm(UserCreationForm):
     first_name = forms.CharField(max_length=30)
     last_name = forms.CharField(max_length=30)
+    skills = forms.ModelMultipleChoiceField(
+        queryset=Skill.objects.all(),
+        widget=forms.CheckboxSelectMultiple,  # lub SelectMultiple
+        required=False
+    )
 
     class Meta:
         model = CustomUser
-        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']  # Usuń skills
 
     def save(self, commit=True):
         user = super().save(commit=False)
         user.is_employee = True
         if commit:
             user.save()
-            Employee.objects.create(user=user)
+            employee = Employee.objects.create(user=user)
+            # Ustawienie umiejętności dla pracownika
+            employee.skills.set(self.cleaned_data['skills'])
         return user
+
+class EditSkillsForm(forms.ModelForm):
+    skills = forms.ModelMultipleChoiceField(
+        queryset=Skill.objects.all(),
+        widget=forms.CheckboxSelectMultiple,  # lub SelectMultiple
+        required=False  # Pole opcjonalne (możliwość odznaczenia wszystkich)
+    )
+
+    class Meta:
+        model = Employee
+        fields = ['skills']  # Pole, które chcemy edytować
--- employees/urls.py
+++ employees/urls.py
@@ -3,8 +3,9 @@ from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('register/', views.register_employee, name='register_employee'),
-    path('', LoginView.as_view(template_name='employees/login.html'), name='login'),
+    path('login/', LoginView.as_view(template_name='employees/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('profile/', views.employee_profile, name='profile'),
 ]
